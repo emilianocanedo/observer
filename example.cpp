28c3a8416d3f1edb3cc4ec22a63086cbbec425e6
Emiliano Canedo <emilianocanedo@gmail.com>
2015-03-10 12:26:20 -0300

    Update example.cpp
    Change in ObserverHandler

--- example.cpp
+++ example.cpp
@@ -1,42 +1,39 @@
 #include <iostream>
-#include "observer.h"
+#include "Observer.h"
 
-void ObserverHandler()
+void ObserverHandler(int state)
 {
-  std::cout << "I'm observer3" << std::endl;
+	std::cout << "I'm observer 3, and have this state: " << state << std::endl;
 }
 
 int main()
 {
-  Observer<int> *observer1 = new Observer<int>;
-  Observer<int> *observer2 = new Observer<int>;
-  Observer<int> *observer3 = new Observer<int>;
+	Observer<int> *observer1 = new Observer<int>;
+	Observer<int> *observer2 = new Observer<int>;
+	Observer<int> *observer3 = new Observer<int>;
 
-  observer3->Delegate = ObserverHandler;
+	observer3->Delegate = ObserverHandler;
 
-  Subject<int> *subject1 = new Subject<int>;
+	Subject<int> *subject1 = new Subject<int>;
 
-  subject1->AddObserver(observer1);
-  subject1->AddObserver(observer2);
-  subject1->AddObserver(observer3);
+	subject1->AddObserver(observer1);
+	subject1->AddObserver(observer2);
+	subject1->AddObserver(observer3);
 
-  subject1->SetState(3);
+	subject1->SetState(3);
 
-  std::cout << observer1->GetState() << " " 
-            << observer2->GetState() << " " 
-            << observer3->GetState() << std::endl;
+	std::cout << observer1->GetState() << " " << observer2->GetState() << " " << observer3->GetState() << std::endl;
 
-  subject1->RemoveObserver(observer2);
-  subject1->SetState(2);
+	subject1->RemoveObserver(observer2);
 
-  std::cout << observer1->GetState() << " " 
-            << observer2->GetState() << " " 
-            << observer3->GetState() << std::endl;
+	subject1->SetState(4);
 
-  delete observer1;
-  delete observer2;
-  delete observer3;
-  delete subject1;
+	std::cout << observer1->GetState() << " " << observer2->GetState() << " " << observer3->GetState() << std::endl;
 
-  return 0;
+	delete observer1;
+	delete observer2;
+	delete observer3;
+	delete subject1;
+
+	return 0;
 }
